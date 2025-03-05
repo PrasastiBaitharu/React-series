f015d288c73c0ca835d93b3d21aeda7dc0ec6ea2
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-03-05 22:07:11 +0530

    Creating different component and fragment in react

---
+++ CreatingComponent/First_project/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Hello from "./Hello";
+import WelcomeMessage from "./WelcomeMessage";
+function App() {
+  return (
+    // <div>
+    // <React.Fragment>
+    <>
+      <Hello/>
+      <WelcomeMessage/>
+    </>
+    // </React.Fragment>
+    //</div>
+  );
+  // return (
+  //   <React.Fragment>
+  //     <Hello />
+  //     <h1>Welcome to react</h1>
+  //   </React.Fragment>
+  // );
+}
+
+export default App;
+
+
+
---
+++ CreatingComponent/First_project/src/Hello.jsx
@@ -0,0 +1,5 @@
+function Hello() {
+    return <h1>Hello my world</h1>;
+  }
+
+export default Hello;
