76c3664e381d2f513d857aaa4bb525cf5fac0d1c
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-03-26 22:41:38 +0530

    Creating a Todo App

---
+++ TodoApp/22 01/toDoApp/src/components/Todo.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+export default function Todo(){
+    const[todo , setTodo] = useState("");
+    const[todos , setTodos] = useState([]);
+
+    function handleSumbit(e){
+        e.preventDefault();
+        // setTodos(todo) not work as we are copying todo string to an array
+        // setTodos([todo]) not work as we are copying todo string to 0th index of array
+        setTodos([...todos , todo])//spread operator is used to add the existing todo in the list
+        setTodo("");
+    }
+
+    return(
+        <>
+            <form onSubmit={handleSumbit}>
+                <input type="text" value={todo} onChange={(e)=>setTodo(e.target.value)}/>
+                <button type="submit">Add</button>
+            </form>
+            {
+                todos.map((item)=>{
+                    return <h1>{item}</h1>
+                })
+            }
+        </>
+    )
+}
