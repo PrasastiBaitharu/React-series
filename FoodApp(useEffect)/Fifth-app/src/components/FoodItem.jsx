5e456e40152e1c1e1603c81dcc752294e776cacb
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-07-15 23:39:21 +0530

    Creating Food App using useEffect

--- FoodApp(useEffect)/Fifth-app/src/components/FoodItem.jsx
+++
@@ -1,9 +0,0 @@
-export default function FoodItem({ food }) {
-  return (
-    <div className="card">
-      <img src={food.image} className="image"/>
-      <h1>{food.title}</h1>
-      <button>VIEW RECIPE</button>
-    </div>
-  );
-}
