20bda91a8425fcbd1604b5b05729ac73c2e15619
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-07-15 23:34:30 +0530

    Creating Food App using useEffect

---
+++ FoodApp(useEffect)/Fifth-app/src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import styles from "./nav.module.css"
+export default function Nav(){
+    return(
+        <div className={styles.navbar}>
+            <h1>🍔FOOD ITEM🍕</h1>
+            
+        </div>
+    )
+}
