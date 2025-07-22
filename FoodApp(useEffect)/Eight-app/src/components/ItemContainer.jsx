648d4ebd3af4222d0e0ed6a0ace66aaefbb8c6a8
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-07-22 22:14:19 +0530

    Creating foodapp using useEffect

---
+++ FoodApp(useEffect)/Eight-app/src/components/ItemContainer.jsx
@@ -0,0 +1,9 @@
+import styles from "./itemContainer.module.css"
+
+export default function ItemContainer({children}){
+    return(
+        <div className={styles.itemContainer}>
+            {children}
+        </div>
+    )
+}
