946851ed6fb2e30c4de210172d8b7a2762c245d6
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-10-26 23:54:55 +0530

    Creating foodapp

--- FoodApp(useEffect)/Eight-app/src/components/Nav.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./nav.module.css"
-export default function Nav(){
-    return(
-        <div className={styles.navbar}>
-            <h1>🍔FOOD ITEM🍕</h1>
-            
-        </div>
-    )
-}
