93d6c4120c5b90e84eee22f2cbdf44e4892bf2a4
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-11-03 23:52:05 +0530

    Creating foodapp

--- FoodApp(useEffect)/Fourth-app/src/components/FoodItem.jsx
+++
@@ -1,9 +0,0 @@
-export default function FoodItem({ food }) {
-  return (
-    <>
-      {/* <h1>{food.id}</h1> */}
-      <h1>{food.title}</h1>
-      <img src={food.image} alt="" />
-    </>
-  );
-}
