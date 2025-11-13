eec2a1567c0c06ba6239e809ee747647ea5d5dd2
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-11-13 22:36:55 +0530

    Creating foodapp using useEffect

---
+++ FoodApp(useEffect)/Sixth-app/src/App.jsx
@@ -0,0 +1,22 @@
+// import Food from "./components/Food";
+import { useState } from "react";
+import FoodList from "./components/FoodList";
+import Search from "./components/Search";
+import Nav from "./components/Nav";
+import styles from "../src/components/container.module.css";
+import Container from "./components/Container";
+function App() {
+  const [foodData, setFoodData] = useState([]);
+  return (
+    <>
+      <Nav />
+      <Search setFoodData={setFoodData} />
+      <Container>
+        <FoodList foodData={foodData} />
+        <h1>Hello</h1>
+      </Container>
+    </>
+  );
+}
+
+export default App;
