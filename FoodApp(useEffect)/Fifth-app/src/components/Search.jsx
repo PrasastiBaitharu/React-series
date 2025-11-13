cd104fd3fdca43fd46bd9df689b4a3bc0f3ea831
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-11-13 13:52:36 +0530

    Creating food app using useEffect

---
+++ FoodApp(useEffect)/Fifth-app/src/components/Search.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = import.meta.env.VITE_SPOONACULAR_API;
+
+export default function Search({setFoodData}) {
+  const [query, setQuery] = useState("Pizza");
+  useEffect(() => {
+    async function fetchFood() {
+      const data = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const res = await data.json();
+      //console.log(res);
+      setFoodData(res.results);
+      // console.log(foodData)
+    }
+    fetchFood();
+  }, [query]);
+  return (
+    <>
+      <input
+      className="search"
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {/* {console.log(foodData)} */}
+      
+    </>
+  );
+}
